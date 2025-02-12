dd575579ff86668dc2adf4865b187c6fa3891b6a
Shekhu04 <gshikhar04@gmail.com>
2025-02-12 10:48:48 +0530

    send and ignore user card from the feed

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -4,6 +4,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { addFeed } from "../utils/feedSlice";
 import { useEffect } from "react";
 import UserCard from "./UserCard";
+
 const Feed = () => {
   
   const feed = useSelector((store) => store.feed);
@@ -22,6 +23,11 @@ const Feed = () => {
   useEffect(() => {
     getFeed();
   }, []);
+
+   if (!feed) return;
+   if (feed.length <= 0)
+     return <h1 className="flex justify-center my-10">No new users founds!</h1>;
+   
   return (
     feed && (
       <div className="flex justify-center my-10">
